1168a5409b86c0652e7bbbd60384cf143ecb04dc
Berny Linville <bernylinville@devopsthink.org>
2025-12-25 21:40:25 +0800

    feat: initialize Nuxt project with Nuxt UI and basic page structure

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div
+        class="min-h-screen flex flex-col items-center justify-center gap-y-4 bg-linear-to-br from-blue-100 to-amber-100">
+        <h1 class="text-4xl font-bold">SRE Berny Linville 正式出发 🛫</h1>
+        <p class="text-gray-500">我们正在开发一系列有趣的工具，敬请期待！</p>
+        <img src="/image001.jpg" alt="Berny" class="w-auto h-64 rounded-2xl shadow-lg">
+        <UButton to="https://x.com/Dunkwithmyson" variant="outline" class="w-auto justify-center">立即关注 👍</UButton>
+    </div>
+</template>
+
+<script setup></script>
